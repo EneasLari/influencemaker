7e48af1aebf0dbffdbaa098eecb39b9b4584c16a
eneaslar <eneaslari@gmail.com>
2020-07-08 13:55:35 +0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -30,12 +30,15 @@ class Canvas{
     }
 
     TextInputHandler(){
-        //console.log(this)
+        console.log("TEXTINPUTHANDLER")
+        
         var canvas = this.TextCanvasElement
         canvas.width=this.ImageCanvasElement.width;
         canvas.height=this.ImageCanvasElement.height;
+        console.log(canvas.height)
         //console.log("IMAGE CANVAS ELEMENT :"+canvas.width+" "+canvas.height );
-        var ctx = this.SetupCanvas(canvas);
+        //console.log("Dimensions=>Image:"+this.ImageCanvasElement.width+"x"+ this.ImageCanvasElement.height+" Text"+this.TextCanvasElement.width+"x"+ this.TextCanvasElement.height)
+        var ctx =canvas.getContext('2d');//this.SetupCanvas(canvas);
         //console.log("IMAGE CANVAS ELEMENT (AFTER):"+canvas.width+" "+canvas.height );
         if(this.TextInputElement.value){
             this.Text=this.TextInputElement.value;
@@ -45,7 +48,13 @@ class Canvas{
         if(this.ColorWell.value){
             ctx.fillStyle = this.ColorWell.value;
         }
-        ctx.font =this.FontSize+ " "+this.FontType;
+
+        var fontBase = 800
+        var size=this.FontSize.split("px")[0]
+        var ratio = size / fontBase;
+        var cSize = canvas.width * ratio;
+
+        ctx.font =cSize+ "px "+this.FontType;
         ctx.textAlign = "center";
         ctx.fillText(this.Text,canvas.width/2,canvas.height/2);
     }
@@ -55,11 +64,12 @@ class Canvas{
         var that=this;//Here this refers to object itself,we assign it to a variable
         img.onload = function(){
             that.DrawBackGround(img);
+            that.TextInputHandler();
         }
         img.src = URL.createObjectURL(event.target.files[0]);
     }
 
-    SetupCanvas(canvas) {
+    SetupCanvas(canvas,scaleCanvas) {
         // Get the device pixel ratio, falling back to 1.
         
         var dpr = window.devicePixelRatio || 2;
@@ -68,18 +78,19 @@ class Canvas{
         var rect = canvas.getBoundingClientRect();
         // Give the canvas pixel dimensions of their CSS
         // size * the device pixel ratio.
-        if(rect.width!=0 || rect.height!=0){
-          canvas.width = rect.width * dpr;
-          canvas.height = rect.height * dpr;
 
-          canvas.style.width = `${dpr*canvas.width}px`;
-          canvas.style.height = `${dpr*canvas.height}px`
+        // if((rect.width!=0 || rect.height!=0)){
+        //   canvas.width = rect.width * dpr;
+        //   canvas.height = rect.height * dpr;
 
-        }
+        //   canvas.style.width = `${dpr*canvas.width}px`;
+        //   canvas.style.height = `${dpr*canvas.height}px`
+
+        // }
         var ctx = canvas.getContext('2d');
         // Scale all drawing operations by the dpr, so you
         // don't have to worry about the difference.
-        console.log("DPR="+dpr)
+        //console.log("DPR="+dpr)
         ctx.scale(dpr, dpr);
         return ctx;
     }
@@ -108,20 +119,25 @@ class Canvas{
     }
 
     Download(event){
-        var newcanvas = this.ImageCanvasElement
-        var textcanvas =this.TextCanvasElement
+        var newcanvas =document.getElementById("downloading")
         if(this.LoadedImage!=null){
             newcanvas.width=this.LoadedImage.width
             newcanvas.height=this.LoadedImage.height
         }
-        var ctxtodownload=this.SetupCanvas(newcanvas)
+        var ctxtodownload=newcanvas.getContext('2d');//this.SetupCanvas(newcanvas)
         if(this.LoadedImage){
             ctxtodownload.drawImage(this.LoadedImage,0,0);
         }
         if( this.ColorWell.value){
             ctxtodownload.fillStyle = this.ColorWell.value;
         }
-        ctxtodownload.font=this.FontSize+" "+this.FontType;
+
+        var fontBase = 800
+        var size=this.FontSize.split("px")[0]
+        var ratio = size / fontBase;
+        var cSize = newcanvas.width * ratio;
+
+        ctxtodownload.font=cSize+"px "+this.FontType;
         ctxtodownload.textAlign="center";
         ctxtodownload.fillText(this.Text,newcanvas.width/2,newcanvas.height/2);
 
@@ -130,6 +146,8 @@ class Canvas{
         link.download = "socialmedia.png";
         link.href = image;
         link.click();
+        newcanvas.width=0;
+        newcanvas.height=0;
     }
 
 }
